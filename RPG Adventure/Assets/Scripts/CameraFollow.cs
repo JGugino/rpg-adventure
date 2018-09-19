bc4945a9f478176fd59433e1527679b571854f58
Gugino <42822655+JGugino@users.noreply.github.com>
2018-09-18 21:51:26 -0400

    Created Player
    Created player, go nav mesh/agent working. worked on camera following/rotation. Rotation still needs work.

---
+++ RPG Adventure/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public static CameraFollow instance;
+
+    public Transform target;
+
+    public float smoothSpeed = 0.8f, rotateSpeed = 20f, pitch = 2f;
+
+    public Vector3 cameraOffset;
+
+    private Transform cameraTransform;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Start()
+    {
+        cameraTransform = GetComponent<Transform>();
+    }
+
+    private void LateUpdate () {
+        cameraTransform.position = Vector3.Lerp(cameraTransform.position, target.position + cameraOffset, smoothSpeed);
+
+        float direction = Input.GetAxis("Horizontal");
+
+        cameraTransform.RotateAround(target.position, Vector3.up * direction, rotateSpeed * Time.deltaTime);
+
+       // cameraTransform.LookAt(target.position + Vector3.up * pitch);
+    }
+}
